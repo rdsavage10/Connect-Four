a765741227eb4e46979529ab2cb98789f2c9f70c
Rick Savage <47980583+rdsavage10@users.noreply.github.com>
2021-09-28 22:54:43 -0400

    added menu added funny bug in menu animation

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 let cvs = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+let ctx = cvs.getContext('2d');
+let cvs2 = document.getElementById('canvas-2');
+let ctx2 = cvs2.getContext('2d');
 let boardRow = 6;
 let boardCol = 7;
 const SQ = 100;
 const VACANT = 'white';
 let board = [];
+let board2 = [];
 let p1Turn = true;
 let p1Color = "red";
 let p2Color = "yellow";
 let colDiv = document.getElementById("button-container");
+let menuDiv = document.getElementById("menu");
+let gameDiv = document.getElementById("game");
 let arrowButtons;
 let gameOver = false;
 let mousehovering = 0;
+let menu = true;
 
 
 function createButtons() {
@@ -30,40 +36,39 @@ function createButtons() {
   arrowButtons = document.querySelectorAll(".arrow");
 }
 
-function drawBoard(col, row) {
+function drawBoard(col, row, canvas) {
   for (let c = 0; c < col; c++) {
     for (let r = 0; r < row; r++) {
-      drawSquare(c, r + 1, board[c][r]);
+      drawSquare(c, r + 1, board[c][r], canvas);
     }
   }
 }
 
-function drawSquare(x, y, color) {
-
-  ctx.fillStyle = "#32557f";
-  ctx.beginPath();
-  ctx.arc(x*SQ + 0.5*SQ, y*SQ+ 0.5*SQ, 0.33*SQ, 0, 2 * Math.PI);
-  ctx.rect(x*SQ + SQ, y*SQ, -SQ, SQ);
-  ctx.fill();
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = "#32557f";
-  ctx.stroke();
+function drawSquare(x, y, color, canvas) {
+  canvas.fillStyle = "#32557f";
+  canvas.beginPath();
+  canvas.arc(x*SQ + 0.5*SQ, y*SQ+ 0.5*SQ, 0.33*SQ, 0, 2 * Math.PI);
+  canvas.rect(x*SQ + SQ, y*SQ, -SQ, SQ);
+  canvas.fill();
+  canvas.lineWidth = 2;
+  canvas.strokeStyle = "#32557f";
+  canvas.stroke();
 }
 
-function drawPiece(x, y, color) {
-  ctx.fillStyle = color;
-  ctx.strokeStyle = 'black';
-  ctx.beginPath();
-  ctx.arc(x*SQ + 0.5*SQ, y*SQ + 0.5*SQ, 0.33*SQ, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.arc(x*SQ + 0.5*SQ, y*SQ + 0.5*SQ, 0.25*SQ, 0, 2 * Math.PI);
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.arc(x*SQ + 0.5*SQ, y*SQ + 0.5*SQ, 0.15*SQ, 0, 2 * Math.PI);
-  ctx.stroke();
-  ctx.closePath()
+function drawPiece(x, y, color, canvas) {
+  canvas.fillStyle = color;
+  canvas.strokeStyle = 'black';
+  canvas.beginPath();
+  canvas.arc(x*SQ + 0.5*SQ, y*SQ + 0.5*SQ, 0.33*SQ, 0, 2 * Math.PI);
+  canvas.fill();
+  canvas.stroke();
+  canvas.beginPath();
+  canvas.arc(x*SQ + 0.5*SQ, y*SQ + 0.5*SQ, 0.25*SQ, 0, 2 * Math.PI);
+  canvas.stroke();
+  canvas.beginPath();
+  canvas.arc(x*SQ + 0.5*SQ, y*SQ + 0.5*SQ, 0.15*SQ, 0, 2 * Math.PI);
+  canvas.stroke();
+  canvas.closePath()
 }
 
 function drawPreview(x) {
@@ -92,13 +97,14 @@ function drawPreview(x) {
   }
 }
 
-function undrawPiece(x, y) {
-  ctx.fillStyle = VACANT;
-  ctx.strokeStyle = VACANT;
-  ctx.beginPath();
-  ctx.arc(x*SQ + 0.5*SQ, y*SQ + 0.5*SQ, 0.34*SQ, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.stroke();
+function undrawPiece(x, y, canvas) {
+  canvas.lineWidth = 2;
+  canvas.fillStyle = VACANT;
+  canvas.strokeStyle = VACANT;
+  canvas.beginPath();
+  canvas.arc(x*SQ + 0.5*SQ, y*SQ + 0.5*SQ, 0.36*SQ, 0, 2 * Math.PI);
+  canvas.fill();
+  canvas.stroke();
 }
 
 function drop(col) {
@@ -125,9 +131,9 @@ function animatePiece(col, y, color, nextEmpty) {
     let speed = 1/5;
     y = Math.floor(y*100)/100;
     if(y <= boardRow && y <= nextEmpty) {
-      undrawPiece(col, y - speed)
-      drawPiece(col, y, color);
-      drawBoard(boardCol, boardRow);
+      undrawPiece(col, y - speed, ctx)
+      drawPiece(col, y, color, ctx);
+      drawBoard(boardCol, boardRow, ctx);
       window.requestAnimationFrame(()=>{animatePiece(col, y + speed, color, nextEmpty)});
     } else {
           arrowButtons.forEach(e => {e.disabled = false;});
@@ -141,6 +147,59 @@ function animatePiece(col, y, color, nextEmpty) {
               e.dispatchEvent(new MouseEvent('mouseover', { 'bubbles': true }));
             }
           });
+    }
+  }, 1000/60)
+
+}
+
+function menuAnimation() {
+  board = [];
+  for (let c = 0; c < boardCol; c++) {
+    board[c] = [];
+    for (let r = 0; r < boardRow; r++) {
+      board[c][r] = VACANT;
+    }
+  }
+  ctx2.scale(0.2, 0.2)
+  drawBoard(boardCol, boardRow, ctx2);
+  menuDrop()
+}
+
+function menuDrop() {
+  let col = Math.floor(Math.random() * boardCol);
+  let nextEmpty;
+  let color;
+  if (p1Turn === true) {
+    color = p1Color;
+  } else {
+    color = p2Color;
+  }
+  for (var i = board[col].length; i >= 0; i--) {
+    if (board[col][i] == VACANT) {
+      board[col][i] = color;
+      nextEmpty = i + 1;
+      menuAnimatePiece(col, 0, color, nextEmpty)
+      break;
+    }
+  }
+}
+
+function menuAnimatePiece(col, y, color, nextEmpty) {
+  setTimeout(function(){
+    let speed = 1/5;
+    y = Math.floor(y*100)/100;
+    if(y <= boardRow && y <= nextEmpty) {
+      undrawPiece(col, y - speed, ctx2)
+      drawPiece(col, y, color, ctx2);
+      drawBoard(boardCol, boardRow, ctx2);
+      window.requestAnimationFrame(()=>{menuAnimatePiece(col, y + speed, color, nextEmpty)});
+    } else {
+          nextTurn();
+          if (menu) {
+            setTimeout(function() {
+              menuDrop();
+            }, 1000/5)
+          }
     }
   }, 1000/60)
 
@@ -231,11 +290,13 @@ function winCheck(col, row, color) {
 
 
 function newGame() {
+  if (menu) {
+    gameDiv.style.display = "flex";
+    menuDiv.style.display = "none";
+    menu = false;
+  }
   gameOver = false;
-  // dropStart = Date.now();
-  // p = randomPiece();
-  // p2 = randomPiece();
-
+  p1Turn = true;
   board = [];
   for (let c = 0; c < boardCol; c++) {
     board[c] = [];
@@ -244,9 +305,15 @@ function newGame() {
     }
   }
 
-
     createButtons();
-    drawBoard(boardCol, boardRow)
+    drawBoard(boardCol, boardRow, ctx)
+}
+
+function quit() {
+  gameDiv.style.display = "none";
+  menuDiv.style.display = "flex";
+  menu = true;
+  menuAnimation();
 }
 
 colDiv.addEventListener("click", event => {
@@ -277,11 +344,11 @@ colDiv.addEventListener("mouseout", event => {
     }
     mousehovering = 0;
     if (event.target.disabled === false) {
-      undrawPiece(event.target.value - 1, 0);
+      undrawPiece(event.target.value - 1, 0, ctx);
     }
   }
 )
 
 
-
-newGame();
+//Title icon
+menuAnimation();
